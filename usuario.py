ee5d9a8118b1ef2e670fc636381f7eabfba7d2b5
Nasha <ignacia.galaz@ug.uchile.cl>
2025-03-19 18:48:12 -0300

    hola mundo

--- usuario.py
+++ usuario.py
@@ -8,9 +8,9 @@ class Usuario:
     def agregarTarea(self, tarea):
         self.tareas.append(tarea)
     
-    def listarTareas(self):
-        for tarea in self.tareas:
-            if tarea.estaLista():
-                print(f"[X] {tarea.obtenerNombre()}" )
-            else:
-                print(f"[ ] {tarea.obtenerNombre()}" )
+def listarTareas(self):
+   for tarea in self.tareas:
+       if tarea.estaLista():
+           print(f"La tarea {tarea.obtenerNombre()} está lista")
+       else: 
+           print(f"La tarea {tarea.obtenerNombre()} no está lista")
